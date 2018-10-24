feff623f011001623df4434342028f3ec3a49f70
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2018-10-24 09:54:39 +0200

    Added jsoup to parse content of Rssfeed entries

--- core/src/main/java/be/ida/medium/parser/MediumRssFeedParser.java
+++ core/src/main/java/be/ida/medium/parser/MediumRssFeedParser.java
@@ -1,7 +1,10 @@
 package be.ida.medium.parser;
 
 import be.ida.medium.model.MediumPost;
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class MediumRssFeedParser {
     public MediumPost syndFeedToMediumPost(SyndFeed syndFeed){
@@ -15,6 +18,13 @@ public class MediumRssFeedParser {
         //.getUri()
 
         //TODO: get photo author en article out of syndFeed
+
+        for (SyndEntry syndEntry: syndFeed.getEntries()){
+            if(!syndEntry.getContents().isEmpty()){
+                Document doc = Jsoup.parseBodyFragment(syndEntry.getContents().get(0).getValue());
+                String publicationImageUrl = doc.select("img").first().attr("src");
+            }
+        }
 
         return mediumPost;
     }
