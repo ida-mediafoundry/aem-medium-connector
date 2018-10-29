9e5136980acd4bf891a4106796b7ed7fb28c4077
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2018-10-29 15:37:29 +0100

    cleanup project + fixed demo page template

--- core/src/test/java/be/ida/medium/connector/MediumConnectorIT.java
+++ core/src/test/java/be/ida/medium/connector/MediumConnectorIT.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class MediumConnectorTest {
+public class MediumConnectorIT {
     MediumConnector mediumConnector = null;
 
     @Before
