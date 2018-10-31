01ca14a74716de960df5c076fbf08081d7eb9a07
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2018-10-31 09:06:56 +0100

    resolved issues with logging dependencies

--- core/src/main/java/be/ida/medium/job/MediumImportJob.java
+++ core/src/main/java/be/ida/medium/job/MediumImportJob.java
@@ -1,18 +1,3 @@
-/*
- *  Copyright 2015 Adobe Systems Incorporated
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package be.ida.medium.job;
 
 import be.ida.medium.connector.MediumConnector;
@@ -36,15 +21,15 @@ public class MediumImportJob implements Runnable {
                            description = "Initiates Medium rss feed import workflow")
     public static @interface Config {
 
-        @AttributeDefinition(name = "Cron-job expression")
+        @AttributeDefinition(name = "scheduler.expression")
         String scheduler_expression() default "*/1 * * * * ?";
 
-        @AttributeDefinition(name = "Concurrent task",
+        @AttributeDefinition(name = "scheduler.concurrent",
                              description = "Whether or not to schedule this task concurrently")
         boolean scheduler_concurrent() default false;
     }
 
-    private final Logger LOG = LoggerFactory.getLogger(getClass());
+    private static final Logger LOG = LoggerFactory.getLogger(MediumImportJob.class);
 
     MediumConnector mediumConnector = null;
 
