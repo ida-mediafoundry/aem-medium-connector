591705b83ae0bacf585ead81813833c2a0cafd2f
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2018-11-02 12:27:30 +0100

    moved test packages, started implementing MediumRepositoryImplIT

--- core/src/test/java/be/ida/medium/repository/MediumRepositoryImplIT.java
+++ core/src/test/java/be/ida/medium/repository/MediumRepositoryImplIT.java
@@ -1,22 +1,19 @@
 package be.ida.medium.repository;
 
-import com.day.cq.wcm.api.Page;
 import io.wcm.testing.mock.aem.junit.AemContext;
-import org.apache.sling.api.resource.Resource;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class JcrMediumRepositoryTest {
+public class MediumRepositoryImplIT {
     @Rule
     public final AemContext context = new AemContext();
 
     @Test
     public void testSomething() {
-        Resource resource = context.resourceResolver().getResource("/content/sample/en");
-        Page page = resource.adaptTo(Page.class);
-        // further testing
+        //TODO: get MediumRepositoryImpl from content and test storeMediumPost
+        //TODO: check if jcr got updated properly
     }
 }
