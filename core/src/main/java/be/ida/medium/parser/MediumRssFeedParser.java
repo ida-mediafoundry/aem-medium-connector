63580de44b439f3b5b414d349a4eb8366a0d4b58
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2019-01-11 08:25:05 +0100

    resolving merge conflicts

--- core/src/main/java/be/ida/medium/parser/MediumRssFeedParser.java
+++ core/src/main/java/be/ida/medium/parser/MediumRssFeedParser.java
@@ -6,7 +6,6 @@ import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.StringUtil;
 import org.jsoup.nodes.Document;
 
 import java.util.ArrayList;
@@ -17,7 +16,7 @@ public class MediumRssFeedParser {
         MediumPublication mediumPublication = new MediumPublication();
         List<MediumPost> mediumPosts = new ArrayList<>();
 
-<<<<<<< HEAD
+
         for (SyndEntry syndEntry : syndFeed.getEntries()){
             MediumPost mediumPost = new MediumPost();
             String mediumPostId = StringUtils.substringAfterLast(syndEntry.getUri(), "/");
@@ -41,29 +40,5 @@ public class MediumRssFeedParser {
         mediumPublication.setPosts(mediumPosts);
         mediumPublication.setName(syndFeed.getTitle());
         return mediumPublication;
-=======
-        if(syndFeed != null){
-            for (SyndEntry syndEntry : syndFeed.getEntries()){
-                MediumPost mediumPublication = new MediumPost();
-                String mediumPublicationId = StringUtils.substringAfterLast(syndEntry.getUri(), "/");
-
-                mediumPublication.setCreator(syndEntry.getAuthor());
-                mediumPublication.setPublicationDate(syndEntry.getPublishedDate().toString());
-                mediumPublication.setLink(syndEntry.getUri());
-                mediumPublication.setTitle(syndEntry.getTitle());
-                mediumPublication.setId(mediumPublicationId);
-
-                if(!syndEntry.getContents().isEmpty()){
-                    Document doc = Jsoup.parseBodyFragment(syndEntry.getContents().get(0).getValue());
-                    String publicationImageUrl = doc.select("img").first().attr("src");
-                    mediumPublication.setImageSource(publicationImageUrl);
-                }
-
-                mediumPublications.add(mediumPublication);
-            }
-        }
-
-        return mediumPublications;
->>>>>>> develop
     }
 }
