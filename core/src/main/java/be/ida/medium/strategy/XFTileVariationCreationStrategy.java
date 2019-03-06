c1df6b700b8d4b28594a2939ea591da0a3145862
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2019-03-06 21:17:45 +0100

    wip

--- core/src/main/java/be/ida/medium/strategy/XFTileVariationCreationStrategy.java
+++ core/src/main/java/be/ida/medium/strategy/XFTileVariationCreationStrategy.java
@@ -1,7 +1,7 @@
 package be.ida.medium.strategy;
 
 public class XFTileVariationCreationStrategy extends AbstractXFVariationCreationStrategy {
-    static final String TEMPLATE_PATH = "/apps/settings/experience-fragments/templates/experience-fragment-template-medium-tile";
+    static final String TEMPLATE_PATH = "/conf/medium/settings/wcm/templates/experience-fragment-template-medium-tile";
     static final String VARIATION_NAME = "medium-tile-variation";
 
     public XFTileVariationCreationStrategy() {
