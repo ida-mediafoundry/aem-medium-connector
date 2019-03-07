6824759ed90eaa5a8a15571d6a2d9dc4ab113bce
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2019-03-07 10:35:23 +0100

    updated page templates

--- core/src/main/java/be/ida/medium/strategy/XFRowVariationCreationStrategy.java
+++ core/src/main/java/be/ida/medium/strategy/XFRowVariationCreationStrategy.java
@@ -1,7 +1,7 @@
 package be.ida.medium.strategy;
 
 public class XFRowVariationCreationStrategy extends AbstractXFVariationCreationStrategy {
-    static final String TEMPLATE_PATH = "/conf/medium/settings/wcm/templates/experience-fragment-template-medium-row/initial";
+    static final String TEMPLATE_PATH = "/conf/medium/settings/wcm/templates/experience-fragment-template-medium-row";
     static final String VARIATION_NAME = "medium-row-variation";
 
     public XFRowVariationCreationStrategy() {
